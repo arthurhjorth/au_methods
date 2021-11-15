8eff922f87c6dfe0957931826fae528337bdc6f3
Arthur Hjorth <arthur@mgmt.au.dk>
2021-11-15 10:35:03 +0100

    Added function to add docs to existing collection

--- add_dataset.py
+++ add_dataset.py
@@ -4,6 +4,26 @@ from models import Document, Collection
 from app import db
 
 
+def add_to_existing(filename, collection_id, first_n=999999999999999999999):
+    with open(filename) as inf:
+        coll = Collection.query.get(collection_id) 
+        counter = 0
+        headings = set()
+        for line in inf.readlines()[:first_n]:
+            counter = counter + 1
+            d = json.loads(line)
+            for key in d:
+                headings.add(key)
+            new_doc = Document(data=d, collection_id=coll.id)
+            db.session.add(new_doc)
+            coll.documents.append(new_doc)
+            if counter % 100000 == 0:
+                print(counter)
+        coll.headings = sorted(list(headings))
+db.session.commit()
+
+
+
 def add(filename, collection_name, first_n=999999999999999999999):
     with open(filename) as inf:
         new_coll = Collection(name=collection_name)
