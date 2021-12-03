f035a95df10064660421f2c4c17ac31428cfe015
Arthur Hjorth <arthur@mgmt.au.dk>
2021-12-03 14:03:54 +0100

    linear reg 1

--- app.py
+++ app.py
@@ -426,37 +426,46 @@ def linear_regression2(collection):
         x = [np.array(data_to_float(t[0])) for t in xy]
         y = [data_to_float(t[1]) for t in xy]
         # Split the data into training/testing sets
-        diabetes_X_train = diabetes_X[:-20]
-        diabetes_X_test = diabetes_X[-20:]
+        # take half here, REMOVE the hard coding
+        x_train = x[:-20]
+        x_test = x[-20:]
 
         # Split the targets into training/testing sets
-        diabetes_y_train = diabetes_y[:-20]
-        diabetes_y_test = diabetes_y[-20:]
+        y_train = y[:-20]
+        y_test = y[-20:]
 
         # Create linear regression object
         regr = linear_model.LinearRegression()
 
         # Train the model using the training sets
-        regr.fit(diabetes_X_train, diabetes_y_train)
+        regr.fit(x_train, y_train)
 
         # Make predictions using the testing set
-        diabetes_y_pred = regr.predict(diabetes_X_test)
+        y_pred = regr.predict(x_test)
 
         # The coefficients
         print("Coefficients: \n", regr.coef_)
         # The mean squared error
-        print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+        print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
         # The coefficient of determination: 1 is perfect prediction
-        print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+        print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+
+        fig_name = "Linear regression on " + x_heading + " & " + y_heading + "on " + coll.name
+
+        data = {}
+        data['Coefficients'] = regr.coef_
+        data['MSE'] = mean_squared_error(y_test, y_pred)
+        data['Coefficients of determination'] = r2_score(y_test, y_pred)
+        data['image'] = fig_name
 
         # Plot outputs
-        plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-        plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
+        plt.scatter(x_test, y_test, color="black")
+        plt.plot(x_test, y_pred, color="blue", linewidth=3)
 
         plt.xticks(())
         plt.yticks(())
 
-        plt.show()
+        plt.savefig(fig_name)
         # return redirect(url_for('linear_regression2', collection1=rq_form['collection1'], collection2=rq_form['collection2']))
     linear_regression_form2 = LinearRegressionForm2()
     linear_regression_form2.x_heading.choices = [(str(h), str(h)) for h in coll.headings] 
